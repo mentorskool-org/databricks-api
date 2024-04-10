38d579bfa5cd58bacd52bda1428cc5e4308fd3cb
Burhan523 <burhanuddin@mentorskool.com>
2024-04-10 11:20:01 +0530

    Updated: Download template

--- fetch_catalog_data.py
+++ fetch_catalog_data.py
@@ -1,9 +1,15 @@
+from databricks import sql
 import pandas as pd
 import requests
 import json
 import time
 
-from constant import TOKEN, DATABRICKS_HOST
+from constant import (
+    TOKEN,
+    DATABRICKS_HOST,
+    DATABRICKS_SERVER_HOSTNAME,
+    DATABRICKS_HTTP_PATH,
+)
 
 
 HEADERS = {"Authorization": f"Bearer {TOKEN}"}
@@ -13,15 +19,17 @@ CLUSTER_ID = "0319-085803-wkbtdodi"
 
 # Fetch all catalogs
 def fetch_catalogs():
-    response = requests.get(f"{DATABRICKS_HOST}/api/2.1/unity-catalog/catalogs", headers=HEADERS)
-    
+    response = requests.get(
+        f"{DATABRICKS_HOST}/api/2.1/unity-catalog/catalogs", headers=HEADERS
+    )
+
     # Fetch the data
     data = response.json()
 
     catalogs = []
-    for catalog in data['catalogs']: # [0]["name"]
+    for catalog in data["catalogs"]:  # [0]["name"]
         catalogs.append(catalog["name"])
-    
+
     return catalogs
 
 
@@ -47,9 +55,13 @@ def get_latest_version_table_metadata(
 
 
 def get_table_metadata(
-    catalog_name: str, database_name: str, table_name: str, rollback_version: int, latest_version: int
+    catalog_name: str,
+    database_name: str,
+    table_name: str,
+    rollback_version: int,
+    latest_version: int,
 ) -> pd.DataFrame:
-    
+
     while True:
         cluster_state = get_cluster_state(CLUSTER_ID)
         print(f"The cluster state is: {cluster_state}")
@@ -67,7 +79,7 @@ def get_table_metadata(
 
     # there is no need for rollback
     rollback_done = False
-    if rollback_version==latest_version:
+    if rollback_version == latest_version:
         print("No need for rollback")
         command = f"""
         display(spark.sql("DESCRIBE FORMATTED {catalog_name}.{database_name}.{table_name};"))
@@ -88,10 +100,10 @@ def get_table_metadata(
     while True:
         response_json = get_command_execution_output(CLUSTER_ID, context_id, command_id)
         if response_json.get("status"):
-            if response_json["status"]=="Finished":
+            if response_json["status"] == "Finished":
                 print(response_json)
                 break
-    
+
     # Fetch the data from this response
     data = response_json["results"]["data"]
 
@@ -110,26 +122,24 @@ def get_table_metadata(
 
         # fetch result and wait until the status == finished, then move ahead
         while True:
-            response_json = get_command_execution_output(CLUSTER_ID, context_id, command_id)
+            response_json = get_command_execution_output(
+                CLUSTER_ID, context_id, command_id
+            )
             if response_json.get("status"):
-                if response_json["status"]=="Finished":
+                if response_json["status"] == "Finished":
                     print(response_json)
                     break
-    
+
     # There is an empty list between table metadata and table properties. Use that empty list and fetch only the metadata
-    metadata_dict = {
-        "columns": [],
-        "dtypes": [],
-        "comments": []
-    }
+    metadata_dict = {"columns": [], "dtypes": [], "comments": []}
     for record in data:
         # Now identify that if all the index in a record is empty means, table metadata is over, so break the loop
-        if record[0]=="" and record[1]=="" and record[2]=="":
+        if record[0] == "" and record[1] == "" and record[2] == "":
             break
-        
-        metadata_dict["columns"].append(record[0]) # Column Name
-        metadata_dict["dtypes"].append(record[1]) # Column Data type
-        metadata_dict["comments"].append(record[2]) # Column Comment
+
+        metadata_dict["columns"].append(record[0])  # Column Name
+        metadata_dict["dtypes"].append(record[1])  # Column Data type
+        metadata_dict["comments"].append(record[2])  # Column Comment
 
     metadata_df = pd.DataFrame(metadata_dict)
 
@@ -196,27 +206,26 @@ def fetch_volumes(schema_name: str, catalog_name: str) -> list:
         if response.get("results") is None:
             continue
 
-        if response.get('results') is not None:
+        if response.get("results") is not None:
             # return response
             break
 
         # Check if data is available in the response
-        if response['results']["resultType"] == "error":
+        if response["results"]["resultType"] == "error":
             raise Exception(f"Error: {response['results']['summary']}")
-        
- 
+
         # results = response['results']
         # if results.get("data") is not None:
         #     versions = []
         #     for version in results['data']:
         #         versions.append(version[0])
-                
+
         #     return versions
-    data = response['results']['data']
+    data = response["results"]["data"]
     volumes = []
     for volume_data in data:
         volumes.append(volume_data[1])
-    
+
     return volumes
 
 
@@ -224,10 +233,10 @@ def fetch_volume_storage(catalog_name, schema_name, volume_name):
     # get the command info
     response = requests.get(
         f"{DATABRICKS_HOST}/api/2.1/unity-catalog/volumes/{catalog_name}.{schema_name}.{volume_name}",
-        headers=HEADERS
+        headers=HEADERS,
     )
 
-    # 
+    #
     storage_location = response.json()["storage_location"]
 
     return storage_location
@@ -238,7 +247,7 @@ def create_volume(catalog_name: str, schema_name: str, volume_name: str) -> dict
         "catalog_name": catalog_name,
         "schema_name": schema_name,
         "name": volume_name,
-        "volume_type": "MANAGED"
+        "volume_type": "MANAGED",
     }
     response = requests.post(
         f"{DATABRICKS_HOST}/api/2.1/unity-catalog/volumes",
@@ -248,11 +257,13 @@ def create_volume(catalog_name: str, schema_name: str, volume_name: str) -> dict
 
     if response.status_code != 200:
         raise Exception("The volume is unable to create")
-    
+
     return response.json()
 
 
-def table_history(schema_name: str, table_name: str, catalog_name: str = "content_datasets") -> dict[str:str]:
+def table_history(
+    schema_name: str, table_name: str, catalog_name: str = "content_datasets"
+) -> dict[str:str]:
     while True:
         cluster_state = get_cluster_state(CLUSTER_ID)
         print(f"The cluster state is: {cluster_state}")
@@ -280,20 +291,23 @@ def table_history(schema_name: str, table_name: str, catalog_name: str = "conten
         response = get_command_execution_output(CLUSTER_ID, context_id, command_id)
 
         if response.get("status"):
-            if response["status"]=="Finished":
+            if response["status"] == "Finished":
                 data = response["results"]["data"]
                 break
-        
+
         if response.get("results"):
-            if response['results']["resultType"] == "error":
-                raise Exception(f"Error: {response['results']['summary']}") # {'results': {'resultType': 'error', 'summary': '<error_def>'}}
-        
+            if response["results"]["resultType"] == "error":
+                raise Exception(
+                    f"Error: {response['results']['summary']}"
+                )  # {'results': {'resultType': 'error', 'summary': '<error_def>'}}
 
     # Fetch the versions and description
     operation_index = 4
     operation_parameters_index = 5
     user_metadata_index = 13
-    versions = []
+    cort_versions = []
+    ccol_versions = []
+    all_versions = []
     description = []
     for record in data:
         # if record[operation_index] == "RESTORE" or record[operation_index] == "CHANGE COLUMN":
@@ -304,29 +318,27 @@ def table_history(schema_name: str, table_name: str, catalog_name: str = "conten
         #     # So we have to reframe the description
         #     operational_paremeter_data = json.loads(record[operation_parameters_index]["column"])
         #     description.append(f"The comment **{operational_paremeter_data['metadata']['comment']}** is added to the column **{operational_paremeter_data['name']}**")
-        
-        if record[operation_index] == "CREATE OR REPLACE TABLE AS SELECT":
-            versions.append(record[0])
-            description.append(record[user_metadata_index])
-    
-    # Serialize the version properly for user's understandability. 
-    # Current actual version number are displayed ex: [15, 2, 1, 0],
-    # this will confuse users that after 2, direct 15 version came where 
-    # are other versions, but in actual we want that other operations are of 
-    # comments orany other operations. The actual versions showed to user should be
-    # versions = [3, 2, 1, 0]
-    versions.sort()
-    for i in range(len(versions)):
-        versions[i] = i
 
-    # reverse the versions list, to sort it from highest to lowest, here 15 will be 
-    # replaced by 3 based on the above example
-    versions = versions[::-1]
-    
-    version_description_map = dict(zip(versions, description))
-    return version_description_map
-    
-    
+        if (
+            record[operation_index] == "CREATE OR REPLACE TABLE AS SELECT"
+            or record[operation_index] == "CREATE TABLE AS SELECT"
+        ):
+            cort_versions.append(record[0])
+            all_versions.append(record[0])
+            description.append(record[user_metadata_index])
+        elif record[operation_index] == "CHANGE COLUMN":
+            ccol_versions.append(record[0])
+            all_versions.append(record[0])
+            
+
+    versions_index = []
+    for i in range(len(cort_versions)):
+        versions_index.append(i)
+
+    version_description_map = dict(zip(cort_versions, description))
+    return versions_index, version_description_map, cort_versions, ccol_versions, all_versions
+
+
 def get_cluster_state(cluster_id: str) -> str | int:
     response = requests.get(
         f"{DATABRICKS_HOST}/api/2.0/clusters/get",
@@ -359,7 +371,9 @@ def create_execution_context(cluster_id: str, language: str) -> str:
     return context_response.json()["id"]
 
 
-def execute_command(cluster_id: str, context_id: str, command: str, language: str) -> str:
+def execute_command(
+    cluster_id: str, context_id: str, command: str, language: str
+) -> str:
     data = {
         "clusterId": cluster_id,
         "contextId": context_id,
@@ -399,12 +413,10 @@ def get_command_execution_output(
     return response.json()
 
 
-def get_total_rows(
-    catalog_name: str, database_name: str, table_name: str
-):
+def get_total_rows(catalog_name: str, database_name: str, table_name: str):
     # Create the context
     context_id = create_execution_context(CLUSTER_ID, "python")
-    
+
     command = f"""
     spark.sql("SELECT COUNT(*) as total_rows FROM {catalog_name}.{database_name}.{table_name}").collect()[0]['total_rows']
 """
@@ -429,7 +441,7 @@ def fetch_table_data(
     database_name: str,
     table_name: str,
     total_rows: int,
-    version: int
+    version: int,
 ):
     # Create the context
     context_id = create_execution_context(CLUSTER_ID, "python")
@@ -523,6 +535,45 @@ def main(
     # }
 
 
+def select_ccol_version(selected_version, cort_versions, ccol_versions):
+    available_versions = []
+    for version in cort_versions:
+        if version == selected_version:
+            for cl_version in ccol_versions:
+                if cl_version > selected_version:
+                    available_versions.append(cl_version)
+        elif version > selected_version:
+            for cl_version in ccol_versions:
+                if cl_version in available_versions and cl_version > version:
+                    available_versions.remove(cl_version)
+
+    if not available_versions:
+        return selected_version
+
+    meta_version = max(available_versions)
+    return meta_version
+
+
+def fetch_data_via_databricks_connector(catalog, schema, table, version):
+    with sql.connect(
+        server_hostname=DATABRICKS_SERVER_HOSTNAME,
+        http_path=DATABRICKS_HTTP_PATH,
+        access_token=TOKEN,
+    ) as connection:
+
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM {catalog}.{schema}.{table}@v{version};")
+            result = cursor.fetchall()
+
+            row1 = result[0]
+
+            # fetch the column names
+            cols = row1.asDict().keys()
+
+    df = pd.DataFrame(result, columns=cols)
+    return df
+
+
 if __name__ == "__main__":
     # catalog_name = input("Enter the catalog name: ")
     # database_name = input("Enter the database name: ")
@@ -560,13 +611,29 @@ if __name__ == "__main__":
     # table_data = table_history(schema_name, table_name, catalog_name)
     # print(table_data)
 
-
     # Fetch the metadata of the version 1 of ecommerce.default.customers
     # catalog_name = "ecommerce"
     # schema_name = "default"
     # table_name = "customers"
 
-    # metadata_df = get_table_metadata(catalog_name, schema_name, table_name, "21", "21")
+    # metadata_df = get_table_metadata(
+    #     database_name="default",
+    #     table_name="customers",
+    #     catalog_name="globalmart_ecommerce",
+    #     rollback_version="6",
+    #     latest_version="6"
+    # )
     # print(metadata_df)
 
-    table_history(schema_name='default', table_name='customers', catalog_name='auto_insurance')
+    versions_index, version_description_map, cort_versions, ccol_versions = table_history(
+        schema_name="default", table_name="customers", catalog_name="globalmart_ecommerce"
+    )
+    user_selection = 0
+    selected_version = select_ccol_version(user_selection, cort_versions, ccol_versions)
+
+    # start_time=time.time()
+    # customers_df = fetch_data_via_databricks_connector("banking", "default", "credits", 0)
+    # end_time=time.time()
+
+    # print(customers_df.head())
+    # print(f"Time taken to fetch the customers data is {end_time-start_time} seconds!!")
--- pages/Download_Catalog_Data_Template.py
+++ pages/Download_Catalog_Data_Template.py
@@ -1,35 +1,41 @@
 import streamlit as st
 import fetch_catalog_data as fc
 from azure_data_storage import fetch_files, download_file_from_s3
+
 # from s3_methods import get_s3_data, fetch_files, download_file_from_s3
 import os
+
 
 def main():
 
     # st.title("Databricks Table Fetcher")
 
     st.write(
-    """
+        """
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"> 
 
          # Databricks Table Fetcher! <i class="fa-solid fa-download"></i>
          """,
-    unsafe_allow_html=True,
-)
+        unsafe_allow_html=True,
+    )
 
     # No need of fetching catalogs from API, all the data is going to store in this single catalog
     # catalogs_list = ['content_datasets'] #, 'main', 'system'] #TODO: fetch the catalogs list from api
     catalogs_list = fc.fetch_catalogs()
-    
+
     # Create a dropdown list using st.selectbox
-    catalog_name = st.selectbox("Select a catalog:", catalogs_list, placeholder="Choose an option")
+    catalog_name = st.selectbox(
+        "Select a catalog:", catalogs_list, placeholder="Choose an option"
+    )
     print(catalog_name)
 
     # Fetch all the tables of that particular catalog via API
     schemas = fc.fetch_schemas(catalog_name)
 
     # Create a dropdown list using st.selectbox
-    schema_name = st.selectbox("Select a database:", schemas, placeholder="Choose an option")
+    schema_name = st.selectbox(
+        "Select a database:", schemas, placeholder="Choose an option"
+    )
     print(schema_name)
 
     # Once the schema is selected give the 2 option of tables and volumes
@@ -43,42 +49,84 @@ def main():
         tables = fc.fetch_tables(schema_name, catalog_name)
 
         # Create a dropdown list using st.selectbox
-        table_name = st.selectbox("Select a table:", tables, placeholder="Choose an option")
+        table_name = st.selectbox(
+            "Select a table:", tables, placeholder="Choose an option"
+        )
         print(table_name)
 
         if table_name:
             # Give the version box to select which version data they want
-            table_data = fc.table_history(schema_name, table_name, catalog_name)
-        
+            (
+                versions_index,
+                versions_map_description,
+                cort_versions,
+                ccol_versions,
+                all_versions,
+            ) = fc.table_history(schema_name, table_name, catalog_name)
+            print(versions_index)
+
             # Create a dropdown list using st.selectbox
-            versions = list(table_data.keys())
-            version = st.selectbox("Select the version:", versions, placeholder="Choose an option")
+            versions = list(versions_map_description.keys())[::-1]
+            selected_version_index = st.selectbox(
+                "Select the version:",
+                versions_index[::-1],
+                placeholder="Choose an option",
+            )
+            version = versions[selected_version_index]
 
-        # print(table_data)
-        print(versions)
-        print(version)
-        print(table_data[version])
-        st.info(f"Description: {table_data[version]}")
-        
-        # st.write(f"You selected the table: {table_name}")
-        if st.button("Fetch Data"):
-            # Display spinner while the process is ongoing
-            with st.spinner("Processing..."):
-                # Call fetch_catalog_data main function to fetch data
-                data = fc.main(catalog_name, schema_name, table_name, version)
+            # print(table_data)
+            print(versions)
+            print(version)
+            print(all_versions)
+            print(versions_map_description[version])
+            st.info(f"Description: {versions_map_description[version]}")
+
+            # st.write(f"You selected the table: {table_name}")
+            if st.button("Fetch Data"):
+                # Display spinner while the process is ongoing
+                with st.spinner("Processing..."):
+                    # Call fetch_catalog_data main function to fetch data
+                    # data = fc.main(catalog_name, schema_name, table_name, version)
+                    data = fc.fetch_data_via_databricks_connector(
+                        catalog_name, schema_name, table_name, version
+                    )
+
+                # Display a subset of the data
+                # st.subheader("Table Data:")
+                # st.write(data)
+
+                default_download_path = os.path.expanduser(r"~\Downloads")
+                file_path = os.path.join(
+                    default_download_path, f"{table_name}_{selected_version_index}.csv"
+                )
+                data.to_csv(file_path, index=False)
+
+                st.info(f"The data is successfully downloaded at {file_path}!!")
+
+            if st.button("Fetch Metadata"):
+                # First fetch the metadata_version
+                meta_version = fc.select_ccol_version(
+                    version, cort_versions, ccol_versions
+                )
                 
-            # Display a subset of the data
-            st.subheader("Table Data:")
-            st.write(data)
-        
-        if st.button("Fetch Metadata"):
-            # Display spinner while the process is ongoing
-            with st.spinner("Processing..."):
-                # Call fetch_catalog_data main function to fetch data
-                meta_data = fc.get_table_metadata(catalog_name, schema_name, table_name, version, versions[0])
+                # st.info(f"Latest version: {all_versions[0]}")
+                # st.info(f"Meta version {meta_version}")
 
-            st.subheader("Table Metadata:")
-            st.write(meta_data)
+                # st.stop()
+
+                # Display spinner while the process is ongoing
+                with st.spinner("Processing..."):
+                    # Call fetch_catalog_data main function to fetch data
+                    meta_data = fc.get_table_metadata(
+                        catalog_name, schema_name, table_name, meta_version, versions[0]
+                    )
+
+                st.subheader("Table Metadata:")
+                st.write(meta_data)
+        else:
+            st.info(
+                f"There are no tables available in the {schema_name} schema of the {catalog_name} catalog!!"
+            )
     elif selected_option == "Volumes":
         # Display the selected option
         st.write(f"You selected: {selected_option}")
@@ -86,34 +134,42 @@ def main():
         # Get user input for the Delta table name
         # volume_name = st.text_input("Enter Volume Name:", "test") # default, we can use any other also
         volumes = fc.fetch_volumes(schema_name, catalog_name)
-        volume_name = st.selectbox("Enter Volume Name:", volumes, placeholder="Choose an option")
+        volume_name = st.selectbox(
+            "Enter Volume Name:", volumes, placeholder="Choose an option"
+        )
         print(volume_name)
 
         try:
-            storage_location = fc.fetch_volume_storage(catalog_name, schema_name, volume_name)
+            storage_location = fc.fetch_volume_storage(
+                catalog_name, schema_name, volume_name
+            )
             print(storage_location)
         except:
-            st.write(f"There is no such volume called **{volume_name}** in {catalog_name}.{schema_name}")
+            st.write(
+                f"There is no such volume called **{volume_name}** in {catalog_name}.{schema_name}"
+            )
             return
 
         # Once we got the location, let's fetch the data from that
         total_files = fetch_files(storage_location)
         print(total_files)
-        
+
         # Create a drop down of available files
-        file = st.selectbox("Select the file:", total_files, placeholder="Choose an option")
-        
+        file = st.selectbox(
+            "Select the file:", total_files, placeholder="Choose an option"
+        )
+
         default_download_path = os.path.expanduser(r"~\Downloads")
         local_path = os.path.join(default_download_path, file)
         print(file)
 
-    #     # Example usage
-    #     object_key = f"{key}/{total_files[0]}"  # Specify the object key of the file in S3
-    #     default_download_path = os.path.expanduser(r"~\Downloads")
-    #     local_path = os.path.join(default_download_path, file)
-    #     # local_path = f"C:/Users/burha/Downloads/{total_files[0]}"  # Specify the local path to save the downloaded file
-    #     print(bucket_name)
-    #     print(object_key)
+        #     # Example usage
+        #     object_key = f"{key}/{total_files[0]}"  # Specify the object key of the file in S3
+        #     default_download_path = os.path.expanduser(r"~\Downloads")
+        #     local_path = os.path.join(default_download_path, file)
+        #     # local_path = f"C:/Users/burha/Downloads/{total_files[0]}"  # Specify the local path to save the downloaded file
+        #     print(bucket_name)
+        #     print(object_key)
 
         # Download the file from ADLS
         if st.button("Fetch Data"):
